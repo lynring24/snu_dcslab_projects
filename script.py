9c331849de78beb0fed0c13125023a9c54ac5f50
herin chung <lync2846@gmail.com>
2020-11-05 19:10:05 +0900

    modify for milc

--- script.py
+++ script.py
@@ -7,24 +7,32 @@ if os.environ.get('DISPLAY','') == '':
 import matplotlib.pyplot as plt
 
 
-tmp = pd.read_csv('./bzip2.log', sep='\s+', header=None)
-skip_list = tmp.index[tmp.iloc[:,0].str.contains('top')].to_list()
+#tmp = pd.read_csv('./433.log', sep='\s+', header=None)
+#skip_list = tmp.index[tmp.iloc[:,0].str.contains('top')].to_list()
 
-log = pd.read_csv('./bzip2.log', header=None, sep='\s+', skiprows=skip_list)
+#log = pd.read_csv('./433.log', header=None, sep='\s+', skiprows=skip_list)
+log = pd.read_csv('./433.log', header=None, sep='\s+')
 
 log.columns = ['pid' ,'usr', 'pr','ni', 'virt', 'res','shr', 's', 'cpu', 'mem','duration', 'command']
-log = log[log['command']=='bzip2_base.gcc4']
+#log = log[log['command']=='bzip2_base.gcc4']
+log = log[log['command']=='milc_base.gcc43']
+
+log['physic'] = log.apply(lambda row:(row['res'] + row['shr'])/1024, axis = 1) 
+log['virt'] = log['virt'].apply(lambda x : x/1024)
+print log.virt.max(), log.physic.max()
 
 
-fig, (axc, axm) = plt.subplots(nrows=2, ncols=1, sharex=True)
+#fig, (axc, axm) = plt.subplots(nrows=2, ncols=1, sharex=True)
+fig, axm = plt.subplots()
 
-axc.plot(log.duration, log.cpu, label="CPU")
-axm.plot(log.duration, log.mem, label="MEM")
+#axc.plot(log.duration, log.cpu, label="CPU")
+axm.plot(log.duration, log.virt, ms=2,linestyle='', marker='.',  label="Virtual")
+axm.plot(log.duration, log.physic, ms=2, linestyle='', marker ='.', label="Physical")
 
-axc.set_ylabel("CPU Usage")
-axm.set_ylabel("MEM Usage")
+#axc.set_ylabel("CPU Usage")
+axm.set_ylabel("MEM Usage (MiB)")
 axm.set_xlabel("Duration")
 
-axc.set_title("CPU, Memory Usaage")
-plt.savefig("./plot.png", format="png")
+axm.set_title("Memory Usaage of 433.milc")
+plt.savefig("./433_memory.png", format="png")
 
